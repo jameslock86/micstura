19e04ddb87ea43f693389d9e5608470d9ea26032
Jeff Hernandez <jeffhernandez@Jeffs-MacBook-Air.local>
2018-09-22 14:52:18 -0600

    scroll events added

--- index.js
+++ index.js
@@ -1,4 +1,20 @@
-window.onload = function () {
-     console.log('Its loaded!') 
-     $('#loading-overlay').addClass("loaded").removeClass("loading")
-}
+$(document).ready(()=>{
+    const TIME_OUT_TIME = 1500
+    let waitOn = 2;
+
+    // $(this).scrollTop(0);
+
+    function removeOverlay() {
+        waitOn--;
+        if (waitOn === 0)
+            $('#loading-overlay').addClass('loaded').removeClass('loading');
+    }
+    
+    window.onload = () => {
+        removeOverlay();
+    }
+    
+    setTimeout(function(){ 
+        removeOverlay();
+     }, TIME_OUT_TIME);  
+})
